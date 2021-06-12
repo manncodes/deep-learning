44140193b0e26231f18af4fb4c7d5f596eaa6647
manncodes <manncodes@gmail.com>
2021-06-12 21:47:22 +0530

    + printing file structure util

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-## My Journey to learn Deep Learning
+# My Journey to learn Deep Learning
+
+## Directory Structure
+
+```bash
+/
+├── Learning Frameworks/
+│   └── PyTorch/
+│       ├── Implementing NN/
+│       │   ├── 1-simple-NN.ipynb
+│       │   ├── 2-CNN.ipynb
+│       │   └── 3-RNN-LSTM-GRU.ipynb
+│       ├── README.md
+│       └── Tensor Tutorial/
+│           ├── initializing_tensor.ipynb
+│           └── other_functionality_of_tensors.ipynb
+├── README.md
+└── tree_copy.py
+```
---
+++ tree_copy.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+
+ignore = ['.git','.ipynb_checkpoints','test','dataset']
+
+class DisplayablePath(object):
+    display_filename_prefix_middle = '├──'
+    display_filename_prefix_last = '└──'
+    display_parent_prefix_middle = '    '
+    display_parent_prefix_last = '│   '
+
+    def __init__(self, path, parent_path, is_last):
+        self.path = Path(str(path))
+        self.parent = parent_path
+        self.is_last = is_last
+        if self.parent:
+            self.depth = self.parent.depth + 1
+        else:
+            self.depth = 0
+
+    @property
+    def displayname(self):
+        if self.path.is_dir():
+            return self.path.name + '/'
+        return self.path.name
+
+    @classmethod
+    def make_tree(cls, root, parent=None, is_last=False, criteria=None):
+        root = Path(str(root))
+        criteria = criteria or cls._default_criteria
+
+        displayable_root = cls(root, parent, is_last)
+        yield displayable_root
+
+        children = sorted([path for path in root.iterdir()
+                                   if criteria(path)], key=lambda s: str(s).lower())
+        for count, path in enumerate(children, start=1):
+            is_last = count == len(children)
+            if path.is_dir():
+                yield from cls.make_tree(path,
+                                         parent=displayable_root,
+                                         is_last=is_last,
+                                         criteria=criteria)
+            else:
+                yield cls(path, displayable_root, is_last)
+
+    @classmethod
+    def _default_criteria(cls, path):  # sourcery skip
+        for dir in ignore:
+            if str(path).find(dir) != -1:
+                return False
+        return True
+
+    @property
+    def displayname(self):
+        if self.path.is_dir():
+            return self.path.name + '/'
+        return self.path.name
+
+    def displayable(self):
+        if self.parent is None:
+            return self.displayname
+
+        _filename_prefix = (self.display_filename_prefix_last
+                            if self.is_last
+                            else self.display_filename_prefix_middle)
+
+        parts = ['{!s} {!s}'.format(_filename_prefix,
+                                    self.displayname)]
+
+        parent = self.parent
+        while parent and parent.parent is not None:
+            parts.append(self.display_parent_prefix_middle
+                         if parent.is_last
+                         else self.display_parent_prefix_last)
+            parent = parent.parent
+
+        return ''.join(reversed(parts))
+
+
+paths = DisplayablePath.make_tree(Path('.'))
+tree = [path.displayable() for path in paths]
+
+with open('README.md', 'w+',encoding="utf-8") as f: #r+ does the work of rw
+    f.write('# My Journey to learn Deep Learning\n')
+    f.write('\n')
+    f.write('## Directory Structure\n')
+    f.write('\n')
+    f.write('```bash')
+    f.write('\n')
+    for line in tree:
+        f.write(line+'\n')
+    f.write('```')
+    
