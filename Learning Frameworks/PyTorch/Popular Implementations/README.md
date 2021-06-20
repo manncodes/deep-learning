366cbfd88ca86829ed596e65828fbe27d8e6cff4
manncodes <manncodes@gmail.com>
2021-06-20 23:31:24 +0530

    PyTorch: ResNet implementation

--- Learning Frameworks/PyTorch/Popular Implementations/README.md
+++ Learning Frameworks/PyTorch/Popular Implementations/README.md
@@ -5,3 +5,4 @@
 |      [LeNet](LeNet.py)     | 1989   | http://yann.lecun.com/exdb/publis/pdf/lecun-98.pdf|
 |[VGG](VGG.py)|2015|https://arxiv.org/abs/1409.1556|
 |[InceptionNet](InceptionNet.py)|2014|https://arxiv.org/abs/1409.4842|
+|[ResNet](ResNet.py)|2015|https://arxiv.org/abs/1512.03385|
---
+++ Learning Frameworks/PyTorch/Popular Implementations/ResNet.py
@@ -0,0 +1,145 @@
+# import
+import torch
+from torch import nn
+
+
+class block(nn.Module):
+    def __init__(
+        self, in_channels, intermediate_channels, identity_downsample=None, stride=1
+    ):
+        super(block, self).__init__()
+        self.expansion = 4
+        self.conv1 = nn.Conv2d(
+            in_channels,
+            intermediate_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            bias=False,
+        )
+        self.bn1 = nn.BatchNorm2d(intermediate_channels)
+        self.conv2 = nn.Conv2d(
+            intermediate_channels,
+            intermediate_channels,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False,
+        )
+        self.bn2 = nn.BatchNorm2d(intermediate_channels)
+        self.conv3 = nn.Conv2d(
+            intermediate_channels,
+            intermediate_channels * self.expansion,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            bias=False,
+        )
+        self.bn3 = nn.BatchNorm2d(intermediate_channels * self.expansion)
+        self.relu = nn.ReLU()
+        self.identity_downsample = identity_downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x.clone()
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+
+        if self.identity_downsample is not None:
+            identity = self.identity_downsample(identity)
+
+        x += identity
+        x = self.relu(x)
+        return x
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, layers, image_channels, num_classes):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(
+            image_channels, 64, kernel_size=7, stride=2, padding=3, bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(block, layers[0], out_channels=64, stride=1)
+        self.layer2 = self._make_layer(block, layers[1], out_channels=128, stride=2)
+        self.layer3 = self._make_layer(block, layers[2], out_channels=256, stride=2)
+        self.layer4 = self._make_layer(block, layers[3], out_channels=512, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * 4, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc(x)
+
+        return x
+
+    def _make_layer(self, block, num_res_blocks, out_channels, stride):
+        identity_downsample = None
+        layers = []
+
+        if stride != 1 or self.in_channels != out_channels * 4:
+            identity_downsample = nn.Sequential(
+                nn.Conv2d(
+                    self.in_channels,
+                    out_channels * 4,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False,
+                ),
+                nn.BatchNorm2d(out_channels * 4),
+            )
+        layers.append(
+            block(self.in_channels, out_channels, identity_downsample, stride)
+        )
+
+        # The expansion size is always 4 for ResNet 50,101,152
+        self.in_channels = out_channels * 4
+
+        for _ in range(num_res_blocks - 1):
+            layers.append(
+                block(self.in_channels, out_channels)
+            )  # end of every block 4 times channels then map back to original number of channels
+
+        return nn.Sequential(*layers)
+
+
+def ResNet50(image_channels=3, num_classes=1000):
+    return ResNet(block, [3, 4, 6, 3], image_channels, num_classes)
+
+
+def ResNet101(image_channels=3, num_classes=1000):
+    return ResNet(block, [3, 4, 23, 3], image_channels, num_classes)
+
+
+def ResNet152(image_channels=3, num_classes=1000):
+    return ResNet(block, [3, 8, 36, 3], image_channels, num_classes)
+
+
+if __name__ == "__main__":
+    model = ResNet50().to("cpu")
+    x = torch.randn(2, 3, 224, 224)
+    y = model(x)
+    print(y.shape)
